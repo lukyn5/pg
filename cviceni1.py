3b37647ff628f60ea1536e7e3640f2a88bed69da
lukyn5 <runnabanku@gmail.com>
2025-10-02 08:22:55 +0000

    Implement main function to process user input and display results

--- cviceni1.py
+++ cviceni1.py
@@ -1,1 +1,16 @@
-print("Hello word")
+def main():
+    vstup = input("zadej číslo: ")
+    vstup = int(vstup)
+    vstup *= 3
+    vstup -= 2
+
+    if vstup > 10:
+        print("větší než 10")
+
+    else:
+        print("mendsi")
+
+    print(f"trojnásobek5 čísla je {vstup}")
+
+if __name__ == "__main__":
+    main()
