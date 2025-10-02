b1fc4bb2082810b311fc3b01ef753e4f269e3107
lukyn5 <runnabanku@gmail.com>
2025-10-02 09:25:03 +0000

    fix

--- first.py
+++ first.py
@@ -1,13 +1,13 @@
+cislo = int(input("Zadej číslo: "))
+
 def sudy_nebo_lichy(cislo):
     if cislo % 2 == 0:
         return True
     else:
         return False
     
-
-
 if __name__ == "__main__":
-    if sudy_nebo_lichy(5) == True:
+    if sudy_nebo_lichy(cislo) == True:
         print(f"Číslo {cislo} je sudé")
     else:
         print(f"Číslo {cislo} je liché")
