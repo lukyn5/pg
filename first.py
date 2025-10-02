7cd2149a1ea98ea186b5767c34e384dcd2675ea4
lukyn5 <runnabanku@gmail.com>
2025-10-02 09:47:02 +0000

    fix

--- first.py
+++ first.py
@@ -1,13 +1,14 @@
-cislo = int(input("Zadej číslo: "))
-
 def suda_nebo_licha(cislo):
     if cislo % 2 == 0:
-        return True
-    else:
-        return False
-    
-if __name__ == "__main__":
-    if suda_nebo_licha(cislo) == True:
         print(f"Číslo {cislo} je sudé")
     else:
         print(f"Číslo {cislo} je liché")
+    
+if __name__ == "__main__":
+    # nadefinovaný vstup
+    suda_nebo_licha(5)
+    suda_nebo_licha(1000000)
+    
+    # uživatelský vstup
+    vstup = int(input("Zadej číslo: "))
+    suda_nebo_licha(vstup)
