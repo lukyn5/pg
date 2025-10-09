353d40236affd9ec1659bdc05e648ba7df01909b
lukyn5 <runnabanku@gmail.com>
2025-10-09 08:23:38 +0000

    x

--- cviceni2.py
+++ cviceni2.py
@@ -1,3 +1,49 @@
 
-if __name__ == "__main__":
 
+def prace_se_seznamem():
+
+
+    seznam = [100, 5, 3, 21]
+
+    #vezme 3. prvek a vynásobí ho 2x
+    seznam[2] *= 2
+    #na konci seznamu přidat nový prvek
+    seznam.append(55)
+    #seznam setridime vzestupne
+    seznam.sort()
+    #otoč pořadí prvku v seznamu
+    seznam.reverse()
+
+    print(seznam)
+
+def vrat_treti_prvek(seznam):
+    
+    if len(seznam) >= 3:
+        return seznam[2]
+    else:
+        return None
+    
+def prumer(cisla):
+    return sum(cisla) / len(cisla)
+    
+
+
+
+
+if __name__ == "__main__":
+    #seznam.append(["a", "b", "c"])
+
+    #print(seznam[4][1])
+
+
+ #   x = [1, 2]
+  #  print(vrat_treti_prvek(x)) # vrati None
+
+   # y = [12, 24, 56, 78]
+   # print(vrat_treti_prvek(y)) # vrati 56
+
+
+
+
+    cisla = [1, 2, 3, 4, 5]
+    print(prumer(cisla))
