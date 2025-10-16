8cd26ecaf1a126f6f195d7b5623c8e5a1de916a6
lukyn5 <runnabanku@gmail.com>
2025-10-16 10:19:26 +0000

    d

--- third.py
+++ third.py
@@ -12,25 +12,6 @@ def je_prvocislo(cislo):
     
     return True
 
-       
-    """
-    Funkce overi, zda zadane cislo je nebo neni prvocislo a vrati True nebo False
-
-    Prvocislo je takove cislo vetsi nez 1, ktere neni delitelne zadnym jinym cislem jenom 1 a samo sebou.
-
-    Napoveda jak otestova prvocislo:
-    Cislo 36 vznikne nasobenim:
-    1 * 36
-    2 * 18
-    3 * 12
-    4 * 9
-    6 * 6
-    9 * 4
-    12 * 3
-    18 * 2
-    36 * 1
-    Jak vidite v druhe polovine se dvojice opakuji, tzn. v tomto pripade staci overit delitelnost pouze do 6 (vcetne)
-    """
 
 def vrat_prvocisla(maximum):
     result = []
