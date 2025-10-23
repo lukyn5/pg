91d73d2685d378fc5431a6b6a308247780fcb3e6
lukyn5 <runnabanku@gmail.com>
2025-10-23 06:39:03 +0000

    pr4

---
+++ prednaska4.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+
+    fronta = [] #FIFO
+    fronta.append(10)
+    fronta.append(5)
+    fronta.append(1)
+    fronta.append(20)
+
+    print(fronta.pop(0))
+
+    fronta.append(3)
+
+    print(fronta.pop(0))
+    print(fronta.pop(0))
+    print(fronta.pop(0))
+    print(fronta.pop(0))
+    print(fronta)
