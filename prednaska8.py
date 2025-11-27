dd47e5b30c4b65b37709ea7aadaccc52b1d26165
lukyn5 <runnabanku@gmail.com>
2025-11-27 08:49:56 +0000

    c

--- prednaska8.py
+++ prednaska8.py
@@ -1,47 +1,39 @@
-import json
 import sys
+import json
 
-class Auto:
+class InvalidData(Exception):
+    pass
+
+class Car:
     def __init__(self, znacka, model, barva):
         self.znacka = znacka
         self.model = model
         self.barva = barva
-
-    def __str__(self):
-        return f"Auto: {self.znacka} / {self.model} / {self.barva}"
     
+    def __str__(self):
+        return f'Auto: {self.znacka} / {self.model} / {self.barva}'
+
     @classmethod
     def parse(cls, jdata):
         data = json.loads(jdata)
-        for value in ("znacka", "model", "barva"):
+        for value in ('znacka', 'model', 'barva'):
             if value not in data:
-                raise ValueError(f'Missing {value} in data')
+                raise InvalidData(f'Missing {value} in data')
         return cls(data['znacka'], data['model'], data['barva'])
-    
+
 
 if __name__ == "__main__":
 
-    jdata1 = '{"znacka": "bmw", "model": "x5", "barva": "modra"}'
+    data = [
+        '{"znacka": "bmw", "barva": "modra"}',
+        '{"znacka": "toyota", "model": "corrola", "barva": "cervena"}',
+        '{"znacka": "mercedes", "model": "slk", "barva": "cerna"}',
+        '{"znacka": "trabant", "model": "600"}'
+    ]
 
-    data = json.loads(jdata1)
-
-    if 'znacka' not in data:
-        print('Invalid data: missing "značka"')
-        sys.exit(1)
-
-    if 'model' not in data:
-        print('Invalid data: missing "model"')
-        sys.exit(1)
-
-    if 'barva' not in data:
-        print('Invalid data: missing "barva"')
-        sys.exit(1)
-
-
-    znacka = data['znacka']
-    model = data['model']
-    barva = data['barva']
-
-    auto = Auto.parse(jdata1)
-
-
+    for item in data:
+        try:
+            auto = Car.parse(item)
+            print(auto)
+        except InvalidData as e:
+            print(e)
--- prednaska8_3.py
+++ prednaska8_3.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     ucet = Ucet()
 
     print(ucet.zustatek)
-    ucet.zustatek = -100
+    ucet.zustatek = + 100
     print(ucet.zustatek)
