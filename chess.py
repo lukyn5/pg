ed33c3736b8571e5404fabe3425e6807024805eb
lukyn5 <runnabanku@gmail.com>
2025-11-29 11:04:16 +0000

    chess

--- chess.py
+++ chess.py
@@ -43,7 +43,21 @@ class Piece(ABC):
 
 class Pawn(Piece):
     def possible_moves(self):
-        return []
+        row, col = self.position
+        moves = []
+
+        if self.color == 'white':
+            # Bílý pěšák postupuje vpřed (řádek se zvyšuje)
+            next_row = row + 1
+            if self.is_position_on_board((next_row, col)):
+                moves.append((next_row, col))
+        elif self.color == 'black':
+            # Černý pěšák postupuje vpřed (řádek se snižuje)
+            next_row = row - 1
+            if self.is_position_on_board((next_row, col)):
+                moves.append((next_row, col))
+
+        return moves
     
     def __str__(self):
         return f'Pawn({self.color}) at position {self.position}'
@@ -75,19 +89,95 @@ class Knight(Piece):
 
 
 class Bishop(Piece):
-    pass
+    def possible_moves(self):
+        """
+        Vrací všechny teoreticky možné tahy střelce (diagonální pohyb).
+        """
+        row, col = self.position
+        moves = []
+        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]  # Smernice: (dr, dc)
+
+        for dr, dc in directions:
+            for i in range(1, 8):  # Max 7 polí v daném směru
+                new_row = row + dr * i
+                new_col = col + dc * i
+                move = (new_row, new_col)
+                if self.is_position_on_board(move):
+                    moves.append(move)
+                else:
+                    break  # Konec desky v tomto směru
+
+        return moves
+
+    def __str__(self):
+        return f'Bishop({self.color}) at position {self.position}'
 
 
 class Rook(Piece):
-    pass
+    def possible_moves(self):
+        """
+        Vrací všechny teoreticky možné tahy věže (horizontální a vertikální pohyb).
+        """
+        row, col = self.position
+        moves = []
+        # Smernice: (dr, dc) - nahoru, dolu, doleva, doprava
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+        for dr, dc in directions:
+            for i in range(1, 8):  # Max 7 polí v daném směru
+                new_row = row + dr * i
+                new_col = col + dc * i
+                move = (new_row, new_col)
+                if self.is_position_on_board(move):
+                    moves.append(move)
+                else:
+                    break  # Konec desky v tomto směru
+
+        return moves
+
+    def __str__(self):
+        return f'Rook({self.color}) at position {self.position}'
 
 
 class Queen(Piece):
-    pass
+    def possible_moves(self):
+        """
+        Vrací všechny teoreticky možné tahy dámy (kombinace věže a střelce).
+        """
+        # Dáma má stejné tahy jako věž a střelec dohromady
+        rook_moves = Rook(self.color, self.position).possible_moves()
+        bishop_moves = Bishop(self.color, self.position).possible_moves()
+        return rook_moves + bishop_moves
+
+    def __str__(self):
+        return f'Queen({self.color}) at position {self.position}'
+
 
 
 class King(Piece):
-    pass
+    def possible_moves(self):
+        """
+        Vrací všechny možné tahy krále (jedno pole ve všech směrech).
+        """
+        row, col = self.position
+        moves = []
+        for dr in [-1, 0, 1]:
+            for dc in [-1, 0, 1]:
+                if dr == 0 and dc == 0:  # Pohyb na stejné místo není tah
+                    continue
+                
+                new_row = row + dr
+                new_col = col + dc
+                move = (new_row, new_col)
+
+                if self.is_position_on_board(move):
+                    moves.append(move)
+
+        return moves
+
+    def __str__(self):
+        return f'King({self.color}) at position {self.position}'
+
 
 
 if __name__ == "__main__":
