8e739cd1d3e75347f1c4a9b1fd2c77489c587cde
lukyn5 <runnabanku@gmail.com>
2025-12-04 10:11:44 +0000

    f

--- cviceni9-3.py
+++ cviceni9-3.py
@@ -1,12 +1,15 @@
 from sixth import download_url_and_get_all_hrefs
 from unittest.mock import MagicMock
+import requests
 
 
 def test_download_url_and_get_all_hrefs():
     data = '<html><body><a id="1" href="https://www.jcu.cz">odkaz</a><div><a href="http://ef.jcu.cz/kontakty">kontakty</a></div></body></html>'
 
-    # TODO
+    requests.get = MagicMock(
+        return_value = MagicMock(ok=True, content=data)
+    )
 
-    href = download_url_and_get_all_hrefs('https://www.jcu.cz')
+    href = download_url_and_get_all_hrefs('xxx')
 
     assert href == ['https://www.jcu.cz', 'http://ef.jcu.cz/kontakty']
